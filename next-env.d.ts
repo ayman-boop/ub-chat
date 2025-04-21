4bfc329ef763b7538ed0c06d34823c6274b648fb
Ayman <aymanbiyadi1@gmail.com>
2025-04-21 19:29:02 -0400

    Add files via upload

---
+++ next-env.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="next" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    MONGODB_URI: string;
+    FIREBASE_API_KEY: string;
+    FIREBASE_AUTH_DOMAIN: string;
+    FIREBASE_PROJECT_ID: string;
+    FIREBASE_STORAGE_BUCKET: string;
+    FIREBASE_MESSAGING_SENDER_ID: string;
+    FIREBASE_APP_ID: string;
+    NEXT_PUBLIC_FIREBASE_API_KEY: string;
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    NEXT_PUBLIC_FIREBASE_APP_ID: string;
+  }
+} 
